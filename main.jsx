49c28b2676bfd9ff1a136efd39ee392a1932399f
gheote <61662165+gheote@users.noreply.github.com>
2025-07-04 16:35:33 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import POSApp from "./POSApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <POSApp />
+  </React.StrictMode>
+);
